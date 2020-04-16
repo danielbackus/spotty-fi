874372820e16e9c26a6d06672e5fd3c68cecd4d8
Daniel Backus <danbackus@gmail.com>
2020-04-16 16:09:10 -0400

    Add tests for API status text

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,9 +1,37 @@
+import '@testing-library/jest-dom';
 import * as React from "react";
-import { render } from "@testing-library/react";
+import ky from 'ky';
+import { render, screen, waitForElement } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect'
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders AP", () => {
   const { getByText } = render(<App />);
   const apiStatus = getByText(/The status of the API server/i);
   expect(apiStatus).toBeInTheDocument();
 });
+
+describe("App", () => {
+  let mockAPI: jest.SpyInstance;
+  beforeEach(() => {
+    mockAPI = jest.spyOn(ky, 'get');
+  }) 
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  const okResponse = {
+    statusText: 'OK'
+  } as Response;
+  it("reports OK API status", async () => {
+    mockAPI.mockResolvedValueOnce(okResponse);
+    render(<App />);
+    const statusText = await waitForElement(() => screen.getByText(/The status of the API server is OK./))
+  });
+  it("reports bad API status", async () => {
+    mockAPI.mockImplementationOnce(() => {
+      throw new Error();
+    });
+    render(<App />);
+    const statusText = await waitForElement(() => screen.getByText(/The status of the API server is bad./))
+  });
+})

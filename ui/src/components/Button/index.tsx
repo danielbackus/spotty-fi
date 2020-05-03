19864b887e068fc1d96327b014299018d6e87efb
dupuis2387 <dupuis2387@gmail.com>
2020-05-03 08:46:39 -0400

    Added play,pause,ff,next controls

--- ui/src/components/Button/index.tsx
+++ ui/src/components/Button/index.tsx
@@ -6,6 +6,7 @@ const styles = {
     alignItems: "center",
     justifyContent: "center",
     padding: "1rem",
+    lineHeight: "1rem",
   },
   left: {},
   right: {},
@@ -16,24 +17,35 @@ const Button = ({
   children = React.Children,
   disabled = false,
   text = "",
+  onClickArgs = null,
   ...props
 }: {
   onClick?: Function;
   children?: React.ReactNode;
   disabled?: boolean;
   text?: string;
-}) => (
-  <button
-    disabled={disabled}
-    onClick={() => onClick()}
-    style={styles.button}
-    {...props}
-  >
-    <div className="fa-button-parent" style={styles.left}>
-      {text}
-    </div>
-    {children}
-  </button>
-);
+  onClickArgs?: any;
+}) => {
+  let elementRef: any = null;
+  return (
+    <button
+      disabled={disabled}
+      onClick={() => {
+        onClick(onClickArgs);
+        elementRef.blur();
+      }}
+      style={styles.button}
+      {...props}
+      ref={(buttonDOM: any) => {
+        elementRef = buttonDOM;
+      }}
+    >
+      <div className="fa-button-parent" style={styles.left}>
+        {text}
+      </div>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
